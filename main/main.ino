cd2f5b4c357b32fd5e16b7eecde46f2829899e96
BitcoderCZ <82174496+BitcoderCZ@users.noreply.github.com>
2026-02-10 09:20:00 +0100

    Demo program

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include "epd4in2.h"
+#include "epdpaint.h"
 
+#define COLORED     0
+#define UNCOLORED   1
+
+void setup() {
+  Serial.begin(9600);
+  Epd epd;
+
+  if (epd.Init() != 0) {
+    Serial.print("e-Paper init failed");
+    return;
+  }
+
+  // This clears the SRAM of the e-paper display
+  epd.ClearFrame();
+
+  /**
+    * Due to RAM not enough in Arduino UNO, a frame buffer is not allowed.
+    * In this case, a smaller image buffer is allocated and you have to 
+    * update a partial display several times.
+    * 1 byte = 8 pixels, therefore you have to set 8*N pixels at a time.
+    */
+  unsigned char image[1500];
+  Paint paint(image, 400, 28);    //width should be the multiple of 8 
+
+  paint.Clear(UNCOLORED);
+  paint.DrawStringAt(0, 0, "e-Paper Demo", &Font24, COLORED);
+  epd.SetPartialWindow(paint.GetImage(), 100, 40, paint.GetWidth(), paint.GetHeight());
+
+
+  // This displays the data from the SRAM in e-Paper module
+  epd.DisplayFrame();
+
+  // Deep sleep
+  epd.Sleep();
 }
 
 void loop() {
