f81dabd39042dad898b2d6bb3c0c3ad8d95116f8
BitcoderCZ <82174496+BitcoderCZ@users.noreply.github.com>
2026-02-10 14:18:36 +0100

    Departures

--- Arduino_R4/Arduino_R4.ino
+++ Arduino_R4/Arduino_R4.ino
@@ -11,14 +11,6 @@ const char* password = "welcome2jic";
 
 UBYTE* ScreenImage;
 
-// NTP Settings
-unsigned int localPort = 2390;
-IPAddress timeServerIP;
-const char* ntpServerName = "pool.ntp.org";
-const int NTP_PACKET_SIZE = 48;
-byte packetBuffer[NTP_PACKET_SIZE];
-WiFiUDP Udp;
-
 void setup() {
   if (DEV_Module_Init() != 0) {
     return;
@@ -27,9 +19,35 @@ void setup() {
   initDisplay();
   initWifi();
 
-  uddateTopBar(true);
+  EPD_4IN2_V2_Clear();
+
+  uddateTopBar(0, 0, 0, true);
+  updateDeparture("31 8:50 15 min", 0);
+  updateDeparture("77 8:55 20 min", 1);
+  updateDeparture("bla bla", 2);
 }
 
+int hours = 8;
+int minutes = 30;
+int seconds = 15;
+
 void loop() {
- 
+  seconds = seconds + 1;
+  if (seconds == 60) {
+    minutes = minutes + 1;
+    seconds = 0;
+    if (minutes == 60) {
+      hours = hours + 1;
+      minutes = 0;
+      if (hours == 24) {
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+      }
+    }
+  }
+
+  //uddateTopBar(hours, minutes, seconds, true);
+
+  DEV_Delay_ms(500);
 }
--- Arduino_R4/drawing.ino
+++ Arduino_R4/drawing.ino
@@ -1,12 +1,19 @@
-void uddateTopBar(bool toTown) {
-  sFONT* Font = &Font24;
-  const int Margin = 5;
-  const int TopBarHeight = Margin + Font->Height;
+sFONT* TopBarFont = &Font24;
+const int TopBarMargin = 8;
+const int TopBarHeight = TopBarMargin + TopBarFont->Height;
 
+sFONT* ItemFont = &Font20;
+const int ItemMargin = 8;
+const int ItemHeight = ItemMargin + TopBarFont->Height;
+
+void uddateTopBar(int hours, int minutes, int seconds, bool toTown) {
   Paint_NewImage(ScreenImage, EPD_4IN2_V2_WIDTH, TopBarHeight, 0, WHITE);
   Paint_Clear(BLACK);
 
-  Paint_DrawString_EN(Margin, Margin, "08:30", Font, WHITE, BLACK);
+  char timeStr[9];  // "HH:MM:SS" + null terminator
+
+  sprintf(timeStr, "%02d:%02d:%02d", hours, minutes, seconds);
+  Paint_DrawString_EN(TopBarMargin, TopBarMargin, timeStr, TopBarFont, WHITE, BLACK);
 
   const char* dirrectionText;
   if (toTown) {
@@ -14,9 +21,23 @@ void uddateTopBar(bool toTown) {
   } else {
     dirrectionText = "Z mesta";
   }
-  uint16_t dirrectionTextWidth = strlen(dirrectionText) * Font->Width;
 
-  Paint_DrawString_EN(EPD_4IN2_V2_WIDTH - dirrectionTextWidth - Margin, Margin, dirrectionText, Font, WHITE, BLACK);
+  uint16_t dirrectionTextWidth = strlen(dirrectionText) * TopBarFont->Width;
+
+  Paint_DrawString_EN(EPD_4IN2_V2_WIDTH - dirrectionTextWidth - TopBarMargin, TopBarMargin, dirrectionText, TopBarFont, WHITE, BLACK);
 
   EPD_4IN2_V2_PartialDisplay(ScreenImage, 0, 0, EPD_4IN2_V2_WIDTH, TopBarHeight);
+  EPD_4IN2_V2_PartialDisplay(ScreenImage, 0, 0, EPD_4IN2_V2_WIDTH, TopBarHeight);
+}
+
+void updateDeparture(const char* text, int index){
+  Paint_NewImage(ScreenImage, EPD_4IN2_V2_WIDTH, ItemHeight, 0, WHITE);
+  Paint_Clear(WHITE);
+
+  int yPos = TopBarHeight + index * ItemHeight;
+
+  Paint_DrawString_EN(ItemMargin, ItemMargin, text, ItemFont, BLACK, WHITE);
+
+  EPD_4IN2_V2_PartialDisplay(ScreenImage, 0, yPos, EPD_4IN2_V2_WIDTH, yPos + ItemHeight);
+  EPD_4IN2_V2_PartialDisplay(ScreenImage, 0, yPos, EPD_4IN2_V2_WIDTH, yPos + ItemHeight);
 }
